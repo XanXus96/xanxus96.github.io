3c3460b9edc2365edd62084d60e66aaae9f21e5b
xanxus96 <marouane.kafou@uit.ac.ma>
2020-06-25 01:42:02 +0100

    fix clock

--- assets/js/site.js
+++ assets/js/site.js
@@ -18,10 +18,12 @@ pill("#page", {
       indicator.style.display = "none";
     }, 500);
     setCurrentYear()
+    runClock()
   }
 });
 
 setCurrentYear()
+runClock()
 
 function addClass(target, className) {
   target.className = target.className
@@ -41,4 +43,23 @@ function removeClass(indicator, className) {
 
 function setCurrentYear() {
   document.querySelector('#footer_year').innerHTML = new Date().getFullYear()
+}
+
+function runClock() {
+  const deg = 6
+  const hr = document.querySelector('#hr')
+  const mn = document.querySelector('#mn')
+  const sc = document.querySelector('#sc')
+
+  setInterval(() => {
+
+      let day = new Date()
+      let hh = day.getHours() * 30
+      let mm = day.getMinutes() * deg
+      let ss = day.getSeconds() * deg
+      
+      hr.style.transform = `rotate(${(hh)+(mm/12)}deg)`
+      mn.style.transform = `rotate(${mm}deg)`
+      sc.style.transform = `rotate(${ss}deg)`
+  })
 }
